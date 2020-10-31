e7253d9d659c76a891c25a95618ebaca439c41c2
alesalsa10 <alexandersalsa10@gmail.com>
2020-10-30 21:42:02 -0400

    added theme persistence

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Countries from './components/Countries/Countries';
@@ -6,30 +6,38 @@ import Country from './components/Country/Country';
 import Header from './components/Header/Header';
 
 import { ThemeProvider } from 'styled-components';
-import {lightTheme, darkTheme} from './themes/themes'
-import {GlobalStyles} from './themes/global'
+import { lightTheme, darkTheme } from './themes/themes';
+import { GlobalStyles } from './themes/global';
 
 function App() {
-
   const [theme, setTheme] = useState('light');
 
   const toggleTheme = () => {
-    if (theme === 'light'){
-      setTheme('dark')
+    if (theme === 'light') {
+      setTheme('dark');
+      localStorage.setItem('theme','light')
     } else {
-      setTheme('light')
+      setTheme('light');
+      localStorage.setItem('theme', 'dark')
     }
-  }
+  };
+
+  useEffect(() => {
+    const currentTheme = localStorage.getItem('theme');
+    if (currentTheme){
+      (currentTheme === 'light') ? setTheme('dark') : setTheme('light')
+    }
+  }, [])
 
   return (
     <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
       <Router>
-        <GlobalStyles/>
+        <GlobalStyles />
         <Header onClick={toggleTheme} theme={theme} />
         <Switch>
-          <Route path='/:id' children={<Country theme={theme}/>} />
+          <Route path='/:id' children={<Country theme={theme} />} />
           <Route exact path='/'>
-            <Countries theme={theme}/>
+            <Countries theme={theme} />
           </Route>
         </Switch>
       </Router>
